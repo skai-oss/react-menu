0999393186fc52e8de45d6cb5c8a8197ce5ae8f1
Peter Shershov <peter.shershov@kenshoo.com>
2018-08-30 10:10:28 +0300

    Menu activeElement / onSelect functionality

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { PureComponent } from "react";
 import classnames from "classnames";
 
 import styles from "./menu.scss";
 
-export const Menu = ({ className, children }) => (
-  <div className={classnames(styles.menu, className)}>{children}</div>
-);
+export class Menu extends PureComponent {
+  state = {
+    activeElement: undefined
+  };
+
+  onSelect = index => this.setState({ activeElement: index });
+
+  render() {
+    const children = React.Children.map(this.props.children, (child, index) =>
+      React.cloneElement(child, { index, onSelect: this.onSelect })
+    );
+
+    return <div className={classnames(styles.menu, className)}>{children}</div>;
+  }
+}
