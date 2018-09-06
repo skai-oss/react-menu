e9987bd34e719ffdb1101eda5c40bd3f5e2d3b59
Peter Shershov <peter.shershov@kenshoo.com>
2018-09-06 16:18:10 +0300

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 export { default as Menu } from "./components/menu";
 export { default as WithMenuContext } from "./components/with_menu_context";
-export { default as MenuContext } from "./components/menu_context";
 export { default as Button } from "./components/button/button";
 export { default as Dropdown } from "./components/dropdown/dropdown";
 export {
